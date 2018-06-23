972b954de48e22dc0fc014c35a0bf193a0d05068
Kevin van der Burgt <kevin@zdev.com>
2018-06-23 22:23:20 +0200

    Added inventory stuff

--- src/plugins/inventory.js
+++ src/plugins/inventory.js
@@ -1,53 +1,103 @@
 import Plugin from '../plugin';
 
 export default class Inventory extends Plugin {
-  onCommand = (username, command, args) => {
-    if (command !== 'inventory') {
-      return;
-    }
+  reportInventory = () => {
+    const items = this.bot.inventory.items();
 
-    // Display all the bot's inventory items
-    if (args.length === 0) {
-      const items = this.bot.inventory.items();
+    const inventoryString = items.length > 0 ? items.map((item) => {
+      return `${item.count}: ${item.displayName ? item.displayName : item.name}`;
+    }).join(', ') : 'nothing';
 
-      const inventoryStr = items.map((item) => {
-        return `${item.count}: ${item.displayName ? item.displayName : item.name}`;
-      }).join(', ');
+    this.bot.chat(`I have: ${inventoryString} and ${36 - items.length} free slots`);
+  };
 
-      this.bot.chat(`I have: ${items.length > 0 ? inventoryStr : 'nothing'} and ${36 - items.length} free slots.`);
-    }
+  tossInventory = () => {
+    const tossItem = (items) => {
+      if (items.length === 0) {
+        this.bot.chat(`Tossed all my items on the ground.`);
+        return;
+      }
 
-    // Drop all items on the ground
-    else if (args.length === 1 && args[0] === 'toss') {
-      const items = this.bot.inventory.items();
-
-      const toss = (items) => {
-        if (items.length === 0) {
-          this.bot.chat(`Okay, i've tossed all items now.`);
+      this.bot.tossStack(items.shift(), (err) => {
+        if (err) {
+          this.bot.chat(`Unable to toss: ${err}`);
           return;
         }
 
-        this.bot.tossStack(items.shift(), (err) => {
-          if (err) {
-            this.bot.chat(`Unable to toss: ${err}`);
+        tossItem(items);
+      });
+    };
+
+    tossItem(this.bot.inventory.items());
+  };
+
+  depositInventory = () => {
+    const inventory = this.bot.inventory.items();
+
+    if (inventory.length === 0) {
+      this.bot.chat(`I have nothing to deposit.`);
+      return;
+    }
+
+    const target = this.bot.findBlock({
+      matching: [54, 130, 146],
+    });
+
+    if (!target) {
+      this.bot.chat(`I can't find any nearby chests.`);
+      return;
+    }
+
+    const path = this.bot.navigate.findPathSync(target.position, {
+      timeout: 2000,
+      endRadius: 1,
+    });
+
+    this.bot.navigate.walk(path.path, (status) => {
+      if (status !== 'arrived') {
+        this.bot.chat(`Could not reach chest: ${status}.`);
+        return;
+      }
+
+      const chest = this.bot.openChest(target);
+
+      chest.on('open', () => {
+        const deposit = (items) => {
+          if (items.length === 0) {
+            this.bot.chat(`Deposited all my items in the chest.`);
+            chest.close();
             return;
           }
 
-          toss(items);
-        });
-      };
+          const item = items.shift();
 
-      toss(items);
+          chest.deposit(item.type, null, item.count, (err) => {
+            if (err) {
+              this.bot.chat(`Unable to deposit all my items, ${err}.`);
+              chest.close();
+              return;
+            }
+
+            deposit(items);
+          });
+        };
+
+        deposit(inventory);
+      });
+    });
+  };
+
+  onCommand = (username, command, args) => {
+    if (command !== 'inventory' && command !== 'inv') {
+      return;
+    }
+
+    if (args.length === 0) {
+      this.reportInventory();
+    } else if (args.length === 1 && args[0] === 'toss') {
+      this.tossInventory();
+    } else if (args.length === 1 && args[0] === 'deposit') {
+      this.depositInventory();
     }
   };
 };
-
-
-
-// function itemStr(item) {
-//   if (item) {
-//     return (item.displayName ? item.displayName : item.name) + " x " + item.count;
-//   } else {
-//     return "(nothing)";
-//   }
-// }
